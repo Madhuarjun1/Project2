1861d10fdcb9c65ca42100a675e55b5e9c8d41db
madhumadhu <Madhubabu.X.Pamtamgi.-ND@disney.com>
2016-03-30 18:42:15 +0530

    second commit.

--- WebContent/customlib/controllers.js
+++ WebContent/customlib/controllers.js
@@ -4,12 +4,11 @@
 
 angular.module('DemoApp.controllers', ["ui.bootstrap"]).
 controller('vieworUpdateorDelete', function($scope, $window, $http,$compile) {
-	$scope.getPagination=function(){
-		
+	
 		var divElement = angular.element(document.querySelector('#pagecontent'));
 		divElement.empty();
 	    var appendHtml = $compile('<page-Type></page-Type>')($scope);
 	    divElement.append(appendHtml);
-	};
+	
 	
 });
